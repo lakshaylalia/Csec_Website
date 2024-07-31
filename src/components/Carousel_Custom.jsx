7a7509ea8c6278d910cb60e5347cb7e5bc223797
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-08-01 01:46:26 +0530

    gallery navbar added and some minor fixes like auto slide off for events csex logo added and name changed to csec

--- src/components/Carousel_Custom.jsx
+++ src/components/Carousel_Custom.jsx
@@ -1,12 +1,51 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import Autoplay from "embla-carousel-autoplay"
 import events from "../../current_events.json"
 import { Card, CardHeader } from "./ui/card";
-import { Carousel,CarouselContent,CarouselItem } from './ui/carousel';
+import { Carousel, CarouselContent, CarouselItem } from './ui/carousel';
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Star } from 'lucide-react';
+import { Inria_Sans, Inria_Serif } from 'next/font/google';
+import axios from 'axios';
+
+const struct = [
+    { Rank: '1', Name: 'rng_58', Score: '7974' },
+    { Rank: '2', Name: 'Zenith', Score: '6011' },
+    { Rank: '3', Name: 'OrOrZZZ!', Score: '5833' },
+    { Rank: '4', Name: 'Petr team', Score: '5792' },
+    { Rank: '5', Name: 'jcvb_matthew99', Score: '5731' },
+    { Rank: '6', Name: 'Excited', Score: '5403' },
+    { Rank: '7', Name: 'dreamoon_love_AA', Score: '5319' },
+    { Rank: '8', Name: 'step5', Score: '4951' },
+    { Rank: '9', Name: 'W4yneb0t', Score: '4762' },
+    { Rank: '10', Name: 'Gullesnuffs', Score: '4737' }
+]
+const inria = Inria_Sans({
+    display: 'swap',
+    subsets: ['latin'],
+    weight: ["300", "400", "700"]
+})
+const inria2 = Inria_Serif({
+    display: 'swap',
+    subsets: ['latin'],
+    weight: ["300", "400", "700"]
+})
 
 
 const Carousel_Custom = () => {
+    const [user, setUser] = useState(struct)
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    const fetchUser = async () => {
+        const contestId = 1991
+        const res = await axios.post('/api/fetch_user', contestId)
+        setUser(res.data)
+    }
+
+
     return (
         <div className='flex flex-col justify-center'>
             <div className=' flex gap-2 items-center mx-8 mt-12 mb-4 md:mb-6'>
@@ -17,10 +56,74 @@ const Carousel_Custom = () => {
                 </h1>
             </div>
             <main className=' flex h-full  justify-center px-4 py-3'>
-                <div className="bg-gray-500 mr-20 w-full h-screen my-4 ml-4 hidden lg:block">
-                    {/* stats */}
+                <div className=" mr-10 w-full h-screen my-4 ml-4 hidden lg:block">
+                    <h1 className={'text-2xl mb-10 font-bold ' + (inria.className)}>Recent Contest Ranking</h1>
+                    <div className='flex flex-col justify-center items-center gap-1'>
+
+                        <div className='relative'>
+                            <div className=" w-30 h-30 flex absolute rounded-full inset-0 bg-yellow-500 bg-opacity-50 filter blur-lg z-0" />
+                            <Avatar className="w-24 h-24">
+                                <AvatarImage className="z-10 absolute inset-0" src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <h1 className={"text-lg text-center font-bold " + (inria.className)}>{user[0].Name}</h1>
+                            <div className='flex gap-1 justify-center items-center z-10'>
+                                <Star className='w-5 h-5 text-yellow-400' />
+                                <h1 className={"font-semibold " + (inria2.className)}>{user[0].Score}</h1>
+                            </div>
+                        </div>
+
+
+                        <div className='flex justify-around gap-40'>
+                            <div className='relative'>
+                                <div className=" w-30 h-30 absolute rounded-full inset-0 bg-gray-400 bg-opacity-50 filter blur-lg z-0" />
+                                <Avatar className="w-24 h-24">
+                                    <AvatarImage className="z-10 absolute inset-0" src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1 className={"text-lg font-bold text-center " + (inria.className)}>{user[1].Name}</h1>
+                                <div className='flex gap-1 justify-center items-center'>
+                                    <Star className='w-5 h-5 text-gray-500' />
+                                    <h1 className={"font-semibold " + (inria2.className)}>{user[1].Score}</h1>
+                                </div>
+                            </div>
+
+                            <div className='relative'>
+                                <div className=" w-30 h-30 absolute rounded-full inset-0 bg-orange-400 bg-opacity-50 filter blur-lg z-0" />
+                                <Avatar className="w-24 h-24">
+                                    <AvatarImage className="z-10 absolute inset-0" src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1 className={"text-lg font-bold text-center " + (inria.className)}>{user[2].Name}</h1>
+                                <div className='flex gap-1 justify-center items-center'>
+                                    <Star className='w-5 h-5 text-orange-400' />
+                                    <h1 className={"font-semibold " + (inria2.className)}>{user[2].Score}</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='flex flex-col mt-10 w-full'>
+                        {
+
+                            user.map((contestant, index) => (
+                                index < 3 ? null :
+                                    (<div key={index} className={'border-b-2 rounded-full p-4 border-gray-500 flex w-full justify-between '+(inria2.className)}>
+                                        <h1 >
+                                            {contestant.Rank}
+                                        </h1>
+                                        <h1 className={inria.className}>
+                                            {contestant.Name}
+                                        </h1>
+                                        <h1>
+                                            {contestant.Score}
+                                        </h1>
+                                    </div>)
+                            ))
+                        }
+                    </div>
                 </div>
-                <Carousel plugins={[Autoplay({ delay: 2000 })]} className="w-full justify-center items-center max-w-full">
+                <Carousel className="w-full justify-center items-center max-w-full">
                     <CarouselContent>
                         {
                             events.map((event, i) => (
@@ -39,8 +142,72 @@ const Carousel_Custom = () => {
                     </CarouselContent>
                 </Carousel>
 
-                <div className="bg-gray-500 ml-20 my-4 mr-4 hidden lg:block w-full h-screen">
-                    {/* {stats} */}
+                <div className=" ml-10 my-4 mr-4 hidden lg:block w-full h-screen">
+                    <h1 className={'text-2xl mb-10 font-bold ' + (inria.className)}>Overall Contest Ranking</h1>
+                    <div className='flex flex-col justify-center items-center gap-1'>
+
+                        <div className='relative'>
+                            <div className=" w-30 h-30 flex absolute rounded-full inset-0 bg-yellow-500 bg-opacity-50 filter blur-lg z-0" />
+                            <Avatar className="w-24 h-24">
+                                <AvatarImage className="z-10 absolute inset-0" src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <h1 className={"text-lg text-center font-bold " + (inria.className)}>{user[0].Name}</h1>
+                            <div className='flex gap-1 justify-center items-center z-10'>
+                                <Star className='w-5 h-5 text-yellow-400' />
+                                <h1 className={"font-semibold " + (inria2.className)}>{user[0].Score}</h1>
+                            </div>
+                        </div>
+
+
+                        <div className='flex justify-around gap-40'>
+                            <div className='relative'>
+                                <div className=" w-30 h-30 absolute rounded-full inset-0 bg-gray-400 bg-opacity-50 filter blur-lg z-0" />
+                                <Avatar className="w-24 h-24">
+                                    <AvatarImage className="z-10 absolute inset-0" src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1 className={"text-lg font-bold text-center " + (inria.className)}>{user[1].Name}</h1>
+                                <div className='flex gap-1 justify-center items-center'>
+                                    <Star className='w-5 h-5 text-gray-500' />
+                                    <h1 className={"font-semibold " + (inria2.className)}>{user[1].Score}</h1>
+                                </div>
+                            </div>
+
+                            <div className='relative'>
+                                <div className=" w-30 h-30 absolute rounded-full inset-0 bg-orange-400 bg-opacity-50 filter blur-lg z-0" />
+                                <Avatar className="w-24 h-24">
+                                    <AvatarImage className="z-10 absolute inset-0" src="https://github.com/shadcn.png" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <h1 className={"text-lg font-bold text-center " + (inria.className)}>{user[2].Name}</h1>
+                                <div className='flex gap-1 justify-center items-center'>
+                                    <Star className='w-5 h-5 text-orange-400' />
+                                    <h1 className={"font-semibold " + (inria2.className)}>{user[2].Score}</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='flex flex-col mt-10 w-full'>
+                        {
+
+                            user.map((contestant, index) => (
+                                index < 3 ? null :
+                                    (<div key={index} className={'border-b-2 rounded-full p-4 border-gray-500 flex w-full justify-between '+(inria2.className)}>
+                                        <h1>
+                                            {contestant.Rank}
+                                        </h1>
+                                        <h1 className={inria.className}>
+                                            {contestant.Name}
+                                        </h1>
+                                        <h1>
+                                            {contestant.Score}
+                                        </h1>
+                                    </div>)
+                            ))
+                        }
+                    </div>
                 </div>
             </main>
         </div>
