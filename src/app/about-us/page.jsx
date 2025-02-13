a19e894843a2ee34b7a285cc2bd7584afa2e16cd
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-02-13 13:41:48 +0530

    added hod and oic messages

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -39,7 +39,7 @@ const About = () => {
         <motion.p className={`text-xl md:text-2xl ${sansita.className} `} variants={fadeInUp} transition={{ delay: 0.2 }}>
           What makes us unique?
         </motion.p>
-        <div className="flex gap-10">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
           <motion.div
             className="flex flex-col justify-center items-center"
             initial={{ opacity: 0, x: -50 }}
@@ -47,13 +47,13 @@ const About = () => {
             viewport={{ once: true }}
             transition={{ duration: 0.4, delay: 0.4 }}
           >
-            <p className={`text-lg md:text-xl ${sansita.className} `}>
+            <p className="description">
             Our vibrant community is dedicated to exploring the vast world of technology, coding, and innovation. We offer a welcoming space for students of all skill levels to learn, collaborate, and grow together. Our mission is to foster a culture of continuous learning and curiosity, where everyone feels empowered to pursue their passion for technology.
               <br />
               <br />
               From hands-on workshops and coding competitions to guest lectures and hackathons, our activities are designed to inspire and challenge. We provide opportunities for members to engage with industry experts, work on real-world projects, and develop skills that are crucial for their future careers. Whether you are a seasoned coder or just starting your journey, CSEC is your platform to learn, create, and make a lasting impact in the world of technology.
               <br/>
-              <br/>
+              <br />
               Join us to be part of a dynamic community that values innovation, creativity, and collaboration. Together, we strive to push the boundaries of what is possible and make meaningful contributions to the tech world. At CSEC, you will find the support, resources, and inspiration you need to achieve your goals and turn your ideas into reality.
             </p>
           </motion.div>
@@ -70,8 +70,8 @@ const About = () => {
             }}
           >
             <img
-            className="rounded-2xl opacity-75 hidden md:block md:h-[30rem] md:w-[200rem] object-cover"
-              src="csec.png"
+            className="rounded-2xl opacity-75 md:h-[30rem] w-full md:w-[200rem] object-cover"
+              src="https://res.cloudinary.com/dtxjhtjv2/image/upload/v1737840220/csec_q6cmnn.webp"
               alt="CSEC Community"
             />
           </motion.div>
---
+++ src/components/IOCSays.tsx
@@ -0,0 +1,77 @@
+"use client"
+import React from "react"
+import { Poppins, Sansita } from "next/font/google"
+import { motion } from "framer-motion"
+import "../../src/components/css/About.css"
+
+const poppins = Poppins({
+    subsets: ["latin-ext"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-poppins",
+})
+
+const sansita = Sansita({
+    display: "swap",
+    subsets: ["latin"],
+    weight: ["400", "700"],
+})
+
+const fadeInUp = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+}
+
+const HodSay = () => {
+    return (
+        <section className={`about-section ${poppins.variable} p-4 md:p-8`}>
+            <motion.div
+                className="about-container"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 0.4 }}
+            >
+                <div className="bg-[#f4f0eb] flex flex-col md:flex-row justify-center items-center p-6 sm:p-8 md:p-12 lg:p-16 mt-8 md:mt-12 lg:mt-0 gap-10">
+                    <motion.div
+                        className="flex justify-center items-center w-full rounded-xl"
+                        initial={{ opacity: 0, x: -50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 0.4, delay: 0.4 }}
+                    >
+                        <img
+                            src="./ioc.jpg"
+                            className="border rounded-xl w-full md:w-auto md:h-[8rem] object-cover"
+                            alt="HOD"
+                        />
+                    </motion.div>
+                    <motion.div
+                        className="flex flex-col justify-center items-center md:w-2/3"
+                        initial={{ opacity: 0, x: 50 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 0.4, delay: 0.4 }}
+                    >
+                        <div className="max-w-screen-lg w-full">
+                            <h2 className="text-2xl md:text-3xl font-bold tracking-wide pb-4 sm:pb-6 lg:pb-8 text-center md:text-left">
+                                <span className="pl-4">
+                                    Message from OIC
+                                </span>
+                            </h2>
+                            <div className="text-base md:text-lg lg:text-xl tracking-wider text-justify md:text-left">
+                                <p className="font-bold md:text-2xl text-xl">Greetings to all</p>
+                                <br/>
+                                <p className="mt-10 text-lg">
+                                Welcome to the Computer Science Engineers&apos; Community (CSEC)! As the Faculty Coordinator of CSEC, a warm welcome is extended to all members and visitors. CSEC serves as a platform where students can collaborate, innovate, and enhance their technical skills. Through workshops, hackathons, guest lectures, and project-based activities, opportunities are provided for students to grow, learn, and connect with industry professionals, and develop solutions to real-world challenges. Active engagement and contribution to the CSEC are encouraged to help and shape the future of technology. Let&apos;s work together to push boundaries and develop as the next generation of computer science leaders.</p>
+                                <br/>
+                                <span className="text-end font-bold text-lg mt-6 block md:inline-block">- Dr. Jyoti Srivasta</span>
+                            </div>
+                        </div>
+                    </motion.div>
+                </div>
+            </motion.div>
+        </section>
+    )
+}
+
+export default HodSay
